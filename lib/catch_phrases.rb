8d669bb4e520cfdcf7cfb5e588c291bdd71b7a00
Jesse Brown <brownjesse8895@yahoo.com>
2021-01-14 21:49:30 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,4 @@
-def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
-end
-
-def toadstool
-  puts status
+def mario 
+  phrase = It's-a me, Mario!
+  puts phrase 
 end
